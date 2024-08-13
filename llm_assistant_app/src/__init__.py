dcb845ed3ac200528201ef52812baa88c9b95d65
Pierre V-F <pierre@pierrevf.consulting>
2024-08-13 09:52:20 +0200

    Making environment loading more robust

--- llm_assistant_app/src/__init__.py
+++ llm_assistant_app/src/__init__.py
@@ -1,4 +1,10 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# To ensure that the right .env file is used, its path is computed from this file
+_parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_env_file_path = os.path.join(_parent_dir, ".env")
 
 
 class Settings(BaseSettings):
@@ -6,7 +12,10 @@ class Settings(BaseSettings):
     anthropic_api_key: str
     debug: bool = False
 
-    model_config = SettingsConfigDict(env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=_env_file_path,
+        env_file_encoding="utf-8",
+    )
 
 
 # Usage
